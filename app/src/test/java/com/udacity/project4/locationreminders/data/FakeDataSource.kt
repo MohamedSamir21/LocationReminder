3675a03ed5949178ca88ee4e5df44b269ad2c602
mo <mo@gmail.com>
2023-02-05 11:27:40 +0200

    Edit very trivial something.

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -20,7 +20,7 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) 
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
         // Save the reminder.
-        reminders?.add(reminder)
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
@@ -37,7 +37,7 @@ class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) 
 
     override suspend fun deleteAllReminders() {
         // Delete all the reminders.
-        reminders?.clear()
+        reminders.clear()
     }
 
     fun setReturnError(value: Boolean) {
